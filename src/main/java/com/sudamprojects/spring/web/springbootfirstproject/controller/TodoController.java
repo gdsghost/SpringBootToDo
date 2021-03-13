cfdb8f69508d6260562fba0127ed5d0a6367c307
$udam <48345668+gdsghost@users.noreply.github.com>
2021-03-13 20:59:19 +0530

    Static todos added

--- src/main/java/com/sudamprojects/spring/web/springbootfirstproject/controller/TodoController.java
+++ src/main/java/com/sudamprojects/spring/web/springbootfirstproject/controller/TodoController.java
@@ -8,8 +8,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.Date;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
     //Inject automatically
@@ -17,10 +21,21 @@ public class TodoController {
     TodoService service;
 
     @RequestMapping(value="/list-todos", method= RequestMethod.GET)
-    public String showLoginMessage(ModelMap model){
-        model.put("todos",service.retrieveTodos("in28Minutes"));
+    public String showTodos(ModelMap model){
+        String name = (String)model.get("name");
+        model.put("todos",service.retrieveTodos(name));
         return "list-todos";
     }
 
+    @RequestMapping(value="/add-todo", method= RequestMethod.GET)
+    public String addTodo(ModelMap model){
+        return "todo";
+    }
+
+    @RequestMapping(value="/add-todo", method= RequestMethod.POST)
+    public String showAddTodo(ModelMap model, @RequestParam String desc){
+        service.addTodo((String)model.get("name"),desc,new Date(),false);
+        return "redirect:list-todos";
+    }
 
 }
--- src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -1,10 +1,33 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
-    <title>First Web Application</title>
+    <title>Todo's for ${name}</title>
 </head>
 
 <body>
-Here are the list of your to-dos.
-${todos}
+<h1>Your Todos</h1>
+<TABLE>
+    <caption>Your todos are</caption>
+        <thead>
+        <tr>
+            <th>Description</th>
+            <th>Target Date</th>
+            <th>Is it Done?</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${todos}" var="todo">
+            <tr>
+                <td>${todo.desc}</td>
+                <td>${todo.targetDate}</td>
+                <td>${todo.done}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+</TABLE>
+<br/>
+<a href="add-todo">Add a Todo</a>
+
+
 </body>
 </html>
